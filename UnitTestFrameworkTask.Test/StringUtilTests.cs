3e1407b670d1c40c1437ed25ae9a461cb3728d0f
AlexanderLukashuk <upiter465@gmail.com>
2024-01-26 09:36:22 +0600

    Removed warnings

--- UnitTestFrameworkTask.Test/StringUtilTests.cs
+++ UnitTestFrameworkTask.Test/StringUtilTests.cs
@@ -12,49 +12,49 @@ namespace UnitTestFrameworkTask.Test
         public void MaxUnequalConsecutiveChars_WhenEmptyString_ReturnZero()
         {
             int result = StringUtil.MaxConsecutiveChars("");
-            Assert.AreEqual(0, result);
+            Assert.That(result, Is.EqualTo(0));
         }
 
         [Test]
         public void MaxUnequalConsecutiveChars_WhenConsecutiveCharsExist_ReturnMaxCount()
         {
             int result = StringUtil.MaxConsecutiveChars("abcd1234ab");
-            Assert.AreEqual(1, result);
+            Assert.That(result, Is.EqualTo(1));
         }
 
         [Test]
         public void MaxConsecuntiveLatinLetters_WhenEmptyString_ReturnZero()
         {
             int result = StringUtil.MaxConsecuntiveLatinLetters("");
-            Assert.AreEqual(0, result);
+            Assert.That(result, Is.EqualTo(0));
         }
 
         [Test]
         public void MaxConsecutiveChars_WhenConsecutiveCharsExist_ReturnMaxCount()
         {
             int result = StringUtil.MaxConsecutiveChars("aaabbbbccc");
-            Assert.AreEqual(4, result);
+            Assert.That(result, Is.EqualTo(4));
         }
 
         [Test]
         public void MaxConsecuntiveChars_WhenConsecutiveCharsExist_ReturnMaxCount()
         {
             int result = StringUtil.MaxConsecuntiveLatinLetters("abcd1234ab");
-            Assert.AreEqual(4, result);
+            Assert.That(result, Is.EqualTo(4));
         }
 
         [Test]
         public void MaxConsecuntiveDigits_WhenEmpty_ReturnZero()
         {
             int result = StringUtil.MaxConsecuntiveDigits("");
-            Assert.AreEqual(0, result);
+            Assert.That(result, Is.EqualTo(0));
         }
 
         [Test]
         public void MaxConsecuntiveDigits_WhenConsecutiveDigitsExist_ReturnMaxCount()
         {
             int result = StringUtil.MaxConsecuntiveDigits("123aa456");
-            Assert.AreEqual(3, result);
+            Assert.That(result, Is.EqualTo(3));
         }
     }
 }
